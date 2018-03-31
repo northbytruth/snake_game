ca93d5d7a5c8972176f68f2b9e56fe5db34fd770
A <aranwebster@googlemail.com>
2018-03-31 20:24:50 +0100

    add
    update

--- src/main.rs
+++ src/main.rs
@@ -13,53 +13,131 @@ struct Snake {
 }
 
 struct Screen {
-    content: Vec<String>,
     height: i32,
     width: i32,
+}
+
+struct Move {
+    vertical: i32,
+    horizontal: i32,
 }
 
 static SPACE: char = ' ';
 static FOOD: char = '*';
 static BODY: char = 'o';
+static HEAD: char = 'c';
 
-fn drawScreen(screenToDisplay :&Screen, window :&Window){
+
+fn startGame(screen : &mut Screen, content :&mut Vec<String>){
+
+    content.clear();
+    
+    //generate board
+    let mut s = "                   \n".to_string();
+    content.push(s);
+    s = "                   \n".to_string();
+    content.push(s);
+    s = format!("{}{}{}{}{}{}{}{}\n",SPACE,BODY,BODY,BODY,HEAD,SPACE,SPACE,FOOD);
+    content.push(s);
+    s = "                   \n".to_string();
+    content.push(s);
+    s = "                   \n".to_string();
+    content.push(s);
+}
+
+fn addIntroToScreen(screen : &mut Screen, content :&mut Vec<String>){
+
+    content.clear();
+    let mut s = "         ____\n".to_string();
+    content.push(s);
+    s = "         SNAKE\n".to_string();
+    content.push(s);
+    s = "         _____\n".to_string();
+    content.push(s);
+    s = "              \n".to_string();
+    content.push(s);
+    s = "Press p to start \n".to_string();
+    content.push(s);
+}
+
+
+//randomise food and iterate snake movement
+fn iterateScreen(screen :&mut Screen, content :&mut Vec<String>, window :&Window){
+
+    //loop through columns then rows
+
+    //place food
+
+    //check food eaten
+
+    //grow snake if needed
+
+    //move snake
+
+}
+
+fn initialiseSnakeAndFood(screen :&mut Screen, content :&mut Vec<String>, window :&Window){
+
+    //place first food
+
+}
+
+
+fn drawScreen(screen :&mut Screen, content :&mut Vec<String>, window :&Window){
 
     window.clear();
 
-    window.printw("jnyrgdb\n");
-    window.printw("GEGE\n");
-    window.printw("GG\n");
-    window.printw("BB\n");
+    //loop through columns then rows
+
+    for row in content.iter() {
+
+        window.printw(row);
+    }
 
     window.refresh();
 }
 
 fn main() {
 
-    let sc = Screen{ gg: 5 };
+    let mut content = Vec::new();
+
+    let mut foodLocation = Point { x: 0, y: 0 };
+    let mut sc = Screen{ height : 100, width : 100, };
+
     let window = initscr();
-    window.printw("Welcome to Snake\n");
+    
     window.refresh();
     noecho();
+
+    addIntroToScreen(&mut sc, &mut content);
     
     loop {
         match window.getch() {
             Some(Input::Character(keyin)) => {
+                
+                //quit game
                 if keyin == 'q' {
                     break;
                 }
 
-                if keyin == 'g' {
-                    drawScreen(&sc, &window);
+                //start game
+                if keyin == 'p' {
+                    startGame(&mut sc, &mut content);
+                    initialiseSnakeAndFood(&mut sc, &mut content, &window);
                 }
 
-                let origin = Point { x: 0, y: 0 }; // origin: Point
+                
 
                 let prnt = format!("KEYIN: {}", keyin);
                 window.printw(&prnt);
             },
             _ => (),
         }
+
+        //randomise food and iterate snake movement
+        iterateScreen(&mut sc, &mut content, &window);
+
+        drawScreen(&mut sc, &mut content, &window);
     }
     endwin();
 }
